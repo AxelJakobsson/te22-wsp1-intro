7b796b390073ff938e965ffb597d6109abdcffd3
TheHoboDad <axejak056@gmail.com>
2024-09-17 11:04:29 +0200

    commit

--- server.js
+++ server.js
@@ -1,51 +1,84 @@
-import express, { Router } from 'express'
+import express, { Router } from "express";
 
-import nunjucks from 'nunjucks'
+import nunjucks from "nunjucks";
 
-const app = express()
+const app = express();
 
+const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 3000
-
-
-nunjucks.configure('views', {
-    autoescape: true,
-    express: app,
+nunjucks.configure("views", {
+  autoescape: true,
+  express: app,
 });
 
-app.use(express.static ("public"))
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.render("index.njk", {
-        message: "Hello World!",
-        title: "Hem",
-        items: ["A", "B", "C", "D"],
-    })
-})
+  console.log(req.query);
+  const name = req.query.name;
+  console.log(name);
+  res.render("index.njk", {
+    message: `Hej, ${name}`,
+    title: "Hem",
+    items: ["A", "B", "C", "D"],
+  });
+});
+
+app.get("/watch", (req, res) => {
+  const movieID = req.query.v;
+  console.log(movieID);
+
+  const movies = {
+    tt011161: {
+      title: "The Shawshank Redemption",
+      year: 1994,
+    },
+    tt011162: {
+        title: "Film",
+        year: 2008,
+    }
+  };
+  const movie = movies[movieID];
+  console.log(movie);
+
+  res.render("watch.njk", {
+    title: "Watch",
+    movie: movie,
+  });
+});
 
 app.get("/om", (req, res) => {
-    res.render("om.njk", {
-        message: "Om sidan",
-        title: "Om hemsidan",
-    })
-})
+  res.render("om.njk", {
+    message: "Om sidan",
+    title: "Om hemsidan",
+  });
+});
 
 app.get("/test", (req, res) => {
-    res.render("test.njk", {
-        message: "Test",
-        title: "Test",
-    })
-})
-
+  res.render("test.njk", {
+    message: "Test",
+    title: "Test",
+  });
+});
 
 app.get("/readme", (req, res) => {
-    console.log(req)
-    res.json({
-        message: "Hello world!",
+  console.log(req);
+  res.json({
+    message: "Hello world!",
+  });
+});
+
+
+app.get('/ytub', (req, res) => {
+    const ID = req.query.v
+    
+    console.log(ID)
+    res.render('ytub.njk', {
+        title: 'Youtube',
+        youtubeID: ID,
     })
 })
 
-  
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
-  })
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
